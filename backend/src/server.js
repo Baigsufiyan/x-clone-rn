f6da1800053b6eae1a655f3131180ff9f4578b3b
sufiyan <sufiyanbaig0531@gmail.com>
2025-08-15 17:26:38 +0530

    starServer function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,18 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-connectDB();
 
 app.get("/", (req, res) => res.send("Hello from server!"));
 
-app.listen(ENV.PORT,() => console.log("server is up and running on PORT:",ENV.PORT)); 
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+        process.exit(1); // Exit the process with failure
+    }
+    };
+startServer();
